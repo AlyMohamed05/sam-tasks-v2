a0f5c87227b360bbf09bdf1dceba4ebe11d8d5b9
Aly <asecure05@gmail.com>
2022-08-25 18:28:36 +0200

    Implemented Welcome Screen.

--- app/src/main/java/com/example/samtasks/ui/fragments/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/example/samtasks/ui/fragments/welcome/WelcomeFragment.kt
@@ -1,11 +1,15 @@
 package com.example.samtasks.ui.fragments.welcome
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.samtasks.R
 import com.example.samtasks.databinding.WelcomeFragmentBinding
+import com.example.samtasks.ui.activities.auth.AuthActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,6 +28,42 @@ class WelcomeFragment : Fragment() {
             false
         )
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initClickListeners()
+    }
+
+    private fun initClickListeners() {
+        binding.apply {
+            // Continue without login button
+            continueTextButton.setOnClickListener {
+                continueWithoutLogin()
+            }
+            loginButton.setOnClickListener {
+                Intent(activity, AuthActivity::class.java).apply {
+                    startActivity(this)
+                    activity?.finish()
+                }
+            }
+        }
+    }
+
+    /**
+     * Saves user choice to shared preferences and navigates user to Home fragment.
+     */
+    private fun continueWithoutLogin() {
+        activity?.let {
+            it.getSharedPreferences(
+                getString(R.string.shared_preferences_name),
+                Context.MODE_PRIVATE
+            ).edit().apply {
+                putBoolean(getString(R.string.continue_without_login_key), true)
+                apply()
+            }
+            // TODO : Navigate to home fragment
+        }
     }
 
 }
