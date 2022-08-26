441ccbf2393bb5bdbb88b1f4661b173d9226c624
Aly <asecure05@gmail.com>
2022-08-26 20:51:15 +0200

    Implemented Login and Signup

--- app/src/main/java/com/example/samtasks/ui/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/example/samtasks/ui/fragments/login/LoginFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.samtasks.databinding.LoginFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
+
+    private val loginViewModel: LoginViewModel by viewModels()
 
     private lateinit var binding: LoginFragmentBinding
     private lateinit var navController: NavController
@@ -31,6 +35,10 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            lifecycleOwner = this@LoginFragment
+            viewModel = loginViewModel
+        }
         navController = findNavController()
         initClickListeners()
     }
--- app/src/main/java/com/example/samtasks/ui/fragments/signup/SignupFragment.kt
+++ app/src/main/java/com/example/samtasks/ui/fragments/signup/SignupFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.samtasks.databinding.SignupFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignupFragment: Fragment() {
+
+    private val signupViewModel: SignupViewModel by viewModels()
 
     private lateinit var binding: SignupFragmentBinding
 
@@ -24,5 +27,13 @@ class SignupFragment: Fragment() {
             false
         )
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            lifecycleOwner = this@SignupFragment
+            viewModel = signupViewModel
+        }
     }
 }
