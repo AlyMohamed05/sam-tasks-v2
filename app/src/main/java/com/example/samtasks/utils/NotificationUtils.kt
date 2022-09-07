1d0407c0dc8d652e86dc9b79089ddd392128d3d9
Aly <asecure05@gmail.com>
2022-09-07 17:43:06 +0200

    Clicking notification opens home fragment.

--- app/src/main/java/com/example/samtasks/utils/NotificationUtils.kt
+++ app/src/main/java/com/example/samtasks/utils/NotificationUtils.kt
@@ -2,12 +2,17 @@ package com.example.samtasks.utils
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.example.samtasks.R
+import com.example.samtasks.ui.activities.host.HostActivity
 
 private const val GEOFENCE_NOTIFICATION_ID = 0
+
+private const val PENDING_INTENT_ID = 1000
 
 fun NotificationManager.createChannel(
     channelId: String,
@@ -30,12 +35,21 @@ fun NotificationManager.sendGeofenceNotification(
     content: String
 ) {
     val channelId = context.getString(R.string.geofencing_notification_channel_id)
+    val intent = Intent(context, HostActivity::class.java)
+    val pendingIntent = PendingIntent.getActivity(
+        context,
+        PENDING_INTENT_ID,
+        intent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
     val notification = NotificationCompat
         .Builder(context, channelId)
         .run {
             setSmallIcon(R.drawable.ic_todo_foreground)
             setContentTitle(context.getString(R.string.reminder))
             setContentText(content)
+            setContentIntent(pendingIntent)
+            setAutoCancel(true)
             priority = NotificationCompat.PRIORITY_MAX
             build()
         }
